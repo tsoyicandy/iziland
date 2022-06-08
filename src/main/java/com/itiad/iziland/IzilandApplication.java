7c291b2a3725e3284b5af295c3ddef38fe9cf7ca
Sandy Tsoyem <sandytsoyem@gmail.com>
2022-06-08 12:47:01 +0200

    Initial commit

---
+++ src/main/java/com/itiad/iziland/IzilandApplication.java
@@ -0,0 +1,13 @@
+package com.itiad.iziland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzilandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzilandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itiad/iziland/IzilandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itiad.iziland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzilandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
